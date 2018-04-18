d199a7b02e41b792a3f1444cd2fa49bcd4e6be5f
Da-Vanna <angeldavanna@live.com>
2018-04-18 06:27:17 -0400

    Added Courses and Login

--- src/examinationassessmentsys/ExaminationAssessmentSys.java
+++ src/examinationassessmentsys/ExaminationAssessmentSys.java
@@ -29,7 +29,7 @@ public class ExaminationAssessmentSys extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
       
 
         Scene scene = new Scene(root, 1000, 1000);
